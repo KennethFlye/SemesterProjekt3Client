d6a3626fa057f08b85743b50149693f9428a4547
bjoernUCN <103989859+bjoernUCN@users.noreply.github.com>
2023-05-11 14:01:08 +0200

    Front/Home page

--- SemesterProjekt3Client/Form1.cs
+++ SemesterProjekt3Client/Form1.cs
@@ -1,3 +1,6 @@
+using System.Reflection;
+using Input;
+
 namespace SemesterProjekt3Client
 {
     public partial class Form1 : Form
@@ -6,5 +9,27 @@ namespace SemesterProjekt3Client
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FileInfo fileInfo = new FileInfo(System.AppDomain.CurrentDomain.BaseDirectory);
+            DirectoryInfo baseDir = fileInfo.Directory.Parent.Parent.Parent;
+            string path = baseDir.FullName + "\\Images\\tetris-movie-v0-nxkmsyiumera1.png";
+
+            pictureBoxFrontImage.LoadAsync(path);
+            lblTitle.Text = "Vestbjerg Biograf Administratorsystem";
+
+            Font bigFont = new Font("Cascadia Code", 20);
+            lblTitle.Font=bigFont;
+        }
+
+        Input.KeyValueChecker kv = new Input.KeyValueChecker();
+        void Form1_KeyPress(object sender, KeyPressEventArgs e) //Keypress input gets handled here
+        {
+            if (kv.KeyDown(e.KeyChar, "1", this))
+            {
+                MessageBox.Show("Shortcut to button 1");
+            }
+        }        
     }
 }
