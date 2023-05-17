98c0b97777aaa30bf31fc2c471d3c62bf4f4eaad
jeppeSiegfredsen <10438303@ucn.dk>
2023-05-17 09:46:33 +0200

    listview er nu list box

--- SemesterProjekt3Client/GUI/SpecificShowing.Designer.cs
+++ SemesterProjekt3Client/GUI/SpecificShowing.Designer.cs
@@ -39,13 +39,16 @@
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.comboBoxMovieCopy = new System.Windows.Forms.ComboBox();
             this.comboBoxShowRoom = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnUpdate
             // 
-            this.btnUpdate.Location = new System.Drawing.Point(293, 251);
+            this.btnUpdate.Location = new System.Drawing.Point(363, 385);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(94, 29);
+            this.btnUpdate.Size = new System.Drawing.Size(118, 36);
             this.btnUpdate.TabIndex = 0;
             this.btnUpdate.Text = "Opdater";
             this.btnUpdate.UseVisualStyleBackColor = true;
@@ -53,94 +56,125 @@
             // 
             // textBoxShowingId
             // 
-            this.textBoxShowingId.Location = new System.Drawing.Point(137, 12);
+            this.textBoxShowingId.Location = new System.Drawing.Point(171, 15);
+            this.textBoxShowingId.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxShowingId.Name = "textBoxShowingId";
-            this.textBoxShowingId.Size = new System.Drawing.Size(125, 27);
+            this.textBoxShowingId.Size = new System.Drawing.Size(155, 31);
             this.textBoxShowingId.TabIndex = 1;
             this.textBoxShowingId.Text = "Id";
             // 
             // labelID
             // 
             this.labelID.AutoSize = true;
-            this.labelID.Location = new System.Drawing.Point(21, 19);
+            this.labelID.Location = new System.Drawing.Point(26, 24);
+            this.labelID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelID.Name = "labelID";
-            this.labelID.Size = new System.Drawing.Size(24, 20);
+            this.labelID.Size = new System.Drawing.Size(30, 25);
             this.labelID.TabIndex = 2;
             this.labelID.Text = "ID";
             // 
             // labelStartTime
             // 
             this.labelStartTime.AutoSize = true;
-            this.labelStartTime.Location = new System.Drawing.Point(21, 60);
+            this.labelStartTime.Location = new System.Drawing.Point(26, 81);
+            this.labelStartTime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelStartTime.Name = "labelStartTime";
-            this.labelStartTime.Size = new System.Drawing.Size(73, 20);
+            this.labelStartTime.Size = new System.Drawing.Size(51, 25);
             this.labelStartTime.TabIndex = 3;
-            this.labelStartTime.Text = "StartTime";
+            this.labelStartTime.Text = "Dato";
             // 
             // labelIsKidFriendly
             // 
             this.labelIsKidFriendly.AutoSize = true;
-            this.labelIsKidFriendly.Location = new System.Drawing.Point(21, 101);
+            this.labelIsKidFriendly.Location = new System.Drawing.Point(26, 314);
+            this.labelIsKidFriendly.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelIsKidFriendly.Name = "labelIsKidFriendly";
-            this.labelIsKidFriendly.Size = new System.Drawing.Size(93, 20);
+            this.labelIsKidFriendly.Size = new System.Drawing.Size(83, 25);
             this.labelIsKidFriendly.TabIndex = 4;
-            this.labelIsKidFriendly.Text = "IsKidFriendly";
+            this.labelIsKidFriendly.Text = "BørneBio";
             // 
             // labelShowRoom
             // 
             this.labelShowRoom.AutoSize = true;
-            this.labelShowRoom.Location = new System.Drawing.Point(21, 152);
+            this.labelShowRoom.Location = new System.Drawing.Point(26, 198);
+            this.labelShowRoom.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelShowRoom.Name = "labelShowRoom";
-            this.labelShowRoom.Size = new System.Drawing.Size(85, 20);
+            this.labelShowRoom.Size = new System.Drawing.Size(35, 25);
             this.labelShowRoom.TabIndex = 5;
-            this.labelShowRoom.Text = "ShowRoom";
+            this.labelShowRoom.Text = "Sal";
             // 
             // labelMovieCopy
             // 
             this.labelMovieCopy.AutoSize = true;
-            this.labelMovieCopy.Location = new System.Drawing.Point(21, 204);
+            this.labelMovieCopy.Location = new System.Drawing.Point(26, 263);
+            this.labelMovieCopy.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelMovieCopy.Name = "labelMovieCopy";
-            this.labelMovieCopy.Size = new System.Drawing.Size(84, 20);
+            this.labelMovieCopy.Size = new System.Drawing.Size(45, 25);
             this.labelMovieCopy.TabIndex = 6;
-            this.labelMovieCopy.Text = "MovieCopy";
+            this.labelMovieCopy.Text = "Film";
             // 
             // checkBoxIsKidFriendly
             // 
             this.checkBoxIsKidFriendly.AutoSize = true;
-            this.checkBoxIsKidFriendly.Location = new System.Drawing.Point(137, 104);
+            this.checkBoxIsKidFriendly.Location = new System.Drawing.Point(171, 318);
+            this.checkBoxIsKidFriendly.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.checkBoxIsKidFriendly.Name = "checkBoxIsKidFriendly";
-            this.checkBoxIsKidFriendly.Size = new System.Drawing.Size(18, 17);
+            this.checkBoxIsKidFriendly.Size = new System.Drawing.Size(22, 21);
             this.checkBoxIsKidFriendly.TabIndex = 7;
             this.checkBoxIsKidFriendly.UseVisualStyleBackColor = true;
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(137, 60);
+            this.dateTimePicker1.Location = new System.Drawing.Point(171, 75);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(250, 27);
+            this.dateTimePicker1.Size = new System.Drawing.Size(188, 31);
             this.dateTimePicker1.TabIndex = 8;
             // 
             // comboBoxMovieCopy
             // 
             this.comboBoxMovieCopy.FormattingEnabled = true;
-            this.comboBoxMovieCopy.Location = new System.Drawing.Point(137, 204);
+            this.comboBoxMovieCopy.Location = new System.Drawing.Point(171, 255);
+            this.comboBoxMovieCopy.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBoxMovieCopy.Name = "comboBoxMovieCopy";
-            this.comboBoxMovieCopy.Size = new System.Drawing.Size(151, 28);
+            this.comboBoxMovieCopy.Size = new System.Drawing.Size(188, 33);
             this.comboBoxMovieCopy.TabIndex = 9;
             // 
             // comboBoxShowRoom
             // 
             this.comboBoxShowRoom.FormattingEnabled = true;
-            this.comboBoxShowRoom.Location = new System.Drawing.Point(137, 152);
+            this.comboBoxShowRoom.Location = new System.Drawing.Point(171, 190);
+            this.comboBoxShowRoom.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBoxShowRoom.Name = "comboBoxShowRoom";
-            this.comboBoxShowRoom.Size = new System.Drawing.Size(151, 28);
+            this.comboBoxShowRoom.Size = new System.Drawing.Size(188, 33);
             this.comboBoxShowRoom.TabIndex = 10;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker2.Location = new System.Drawing.Point(171, 130);
+            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(188, 31);
+            this.dateTimePicker2.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 136);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 25);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Tid";
             // 
             // SpecificShowing
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(406, 300);
+            this.ClientSize = new System.Drawing.Size(508, 449);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker2);
             this.Controls.Add(this.comboBoxShowRoom);
             this.Controls.Add(this.comboBoxMovieCopy);
             this.Controls.Add(this.dateTimePicker1);
@@ -152,6 +186,7 @@
             this.Controls.Add(this.labelID);
             this.Controls.Add(this.textBoxShowingId);
             this.Controls.Add(this.btnUpdate);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "SpecificShowing";
             this.Text = "SpecificShowing";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SpecificShowing_FormClosing);
@@ -174,5 +209,7 @@
         private DateTimePicker dateTimePicker1;
         private ComboBox comboBoxMovieCopy;
         private ComboBox comboBoxShowRoom;
+        private DateTimePicker dateTimePicker2;
+        private Label label1;
     }
 }
